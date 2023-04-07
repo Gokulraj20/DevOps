423f1bad97cf671f3cd6d6b83eed7879996fe100
Vignesh_2997 <vickygokul5@gmail.com>
2023-04-07 19:31:21 +0530

    Modified terraform main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -6,17 +6,61 @@ resource "aws_vpc" "my_vpc" {
     }
 }
 
-# Create subnet in each AZ
-resource "aws_subnet" "my_subnet" {
-    count = length(var.availability_zones)
+# Create Public and Private subnets in each AZ
+resource "aws_subnet" "Public_subnet" {
+    count = 2
+    //count = length(var.availability_zones)
     cidr_block = "10.0.${count.index + 1}.0/24"
     vpc_id = aws_vpc.my_vpc.id
-    availability_zone = element(var.availability_zones, count.index)
+    //availability_zone = element(var.availability_zones, count.index)
     tags = {
-      Name = "My Subnet ${count.index + 1}"
+      Name = "Public-subnet-${count.index + 1}"
     }
 }
 
-resource "" "name" {
-  
+resource "aws_subnet" "Private_subnet" {
+    count = 2
+    //count = length(var.availability_zones)
+    cidr_block = "10.0.${count.index + 3}.0/24"
+    vpc_id = aws_vpc.my_vpc.id
+    //availability_zone = element(var.availability_zones, count.index)
+    tags = {
+      Name = "Private-subnet-${count.index + 1}"
+    }
+}
+
+# Create Internet Gateway
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.my_vpc.id
+    tags = {
+      Name = "my-igw"
+    }
+}
+
+# Create Nat Gateway
+resource "aws_nat_gateway" "Nat-gw" {
+    count = 2
+    allocation_id = aws_eip.eip[count.index + 1].id
+    subnet_id = aws_subnet.Private_subnet[count.index].id
+    tags = {
+      Name = "nat-gateway-${count.index +1}"
+    }
+}
+
+
+# Route table for Public and Private subnets
+resource "aws_route_table" "PublicRT" {
+    vpc_id = aws_vpc.my_vpc.id
+    route = {
+        cidr_block ="0.0.0.0/0"
+        gateway_id = 
+    }
+}
+
+resource "aws_route_table" "PrivateRT" {
+    vpc_id = aws_vpc.my_vpc.id
+    route = {
+        cidr_block ="0.0.0.0/0"
+        nat_gateway_id = 
+    }
 }
